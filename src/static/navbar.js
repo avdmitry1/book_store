500772498414d10caaf2149462547b91a4654458
avdmitry1 <avdmitry1@gmail.com>
2025-04-15 21:32:15 +0300

    fix menu dropdown button

--- src/static/navbar.js
+++ src/static/navbar.js
@@ -1,16 +1,29 @@
-// const btnDropdown = document.getElementById('btn-dropdown');
-// const dropDownBox = document.getElementById('dropdown-box');
-// const dropdown = document.getElementById('dropdown');
+const btnDropdown = document.getElementById('btn-dropdown');
+const dropdownMenu = document.getElementById('dropdown'); // Меню (список)
+const dropdownBox = document.getElementById('dropdown-box'); // Контейнер меню
 
-// btnDropdown.addEventListener('click', () => {
-//     dropdown.classList.toggle('hidden');
-//     dropdown.innerHTML = dropdown.classList.contains('hidden') ?
-//         'MENU &nbsp; <i class="fa-solid fa-caret-down"></i>' :
-//         'MENU &nbsp; <i class="fa-solid fa-xmark"></i>';
-// });
+if (btnDropdown && dropdownMenu && dropdownBox) {
+    // Константи для тексту кнопки 
+    const MENU_CLOSED_HTML = 'MENU &nbsp; <i class="fa-solid fa-caret-down"></i>';
+    const MENU_OPENED_HTML = 'MENU &nbsp; <i class="fa-solid fa-xmark"></i>';
 
+    btnDropdown.addEventListener('click', () => {
+        const isHidden = dropdownMenu.classList.toggle('hidden');
 
-// dropDownBox.addEventListener('mouseleave', () => {
-//     dropdown.classList.add('hidden');
-//     dropdown.innerHTML = 'MENU &nbsp; <i class="fa-solid fa-caret-down"></i>';
-// });
+        // Змінюємо іконку при натисканні
+        btnDropdown.innerHTML = isHidden ? MENU_CLOSED_HTML : MENU_OPENED_HTML;
+        // Оновлюємо атрибут доступності
+        btnDropdown.setAttribute('aria-expanded', !isHidden);
+    });
+
+    dropdownBox.addEventListener('mouseleave', () => {
+        // Скриваєм меню
+        dropdownMenu.classList.add('hidden');
+        // Повертаємо іконку
+        btnDropdown.innerHTML = MENU_CLOSED_HTML;
+        // Оновлюємо атрибут доступності
+        btnDropdown.setAttribute('aria-expanded', 'false');
+    });
+} else {
+    console.error('Один из элементов не найден');
+}
